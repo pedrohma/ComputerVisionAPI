c55634f4ac100589990980eadec0fcce17c71ea2
Pedro Henrique Arthur <pedrohma95@gmail.com>
2018-03-19 21:31:27 -0700

    Starting

---
+++ Program.cs
@@ -0,0 +1,22 @@
+﻿using System;
+
+namespace ComputerVisionAPI
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello World!");
+
+            Console.Write("Enter the path to an image you wish to analzye for landmarks: ");
+            string imageFilePath = Console.ReadLine();
+
+            AnalysisRequest.MakeAnalysisRequest(imageFilePath);
+
+            Console.WriteLine("\nPlease wait a moment for the results to appear. Then, press Enter to exit ...\n");
+            Console.ReadLine();
+
+            Console.WriteLine(APIKeys.getKey());
+        }
+    }
+}
---
+++ util/APIKeys.cs
@@ -0,0 +1,11 @@
+using System;
+
+namespace ComputerVisionAPI
+{
+    public class APIKeys
+    {
+        public const string Key = "YOUR_API_KEY";
+
+        public static string getKey() => Key;
+    }
+}
